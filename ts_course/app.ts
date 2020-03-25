b99a73fb6f29a0ce01c9084703e26316ff1c975e
aditya uppuluri <aditya.uppuluri@cradlepoint.com>
2020-03-25 17:26:31 +0530

    number, boolean, string and object types

--- ts_course/app.ts
+++ ts_course/app.ts
@@ -1,1 +1,22 @@
-console.log("Your code goes here....!!!")
+function add(n1: number, n2: number, printResult: boolean, phrase: string){
+    const result = n1 + n2;
+    if(printResult){
+        console.log(phrase + result);
+    }
+    return result;
+}
+
+const num1 = 5;
+const num2 = 2.8;
+const printResult = true;
+const phrase = "Result is ";
+
+add(num1, num2, printResult, phrase);
+
+/** object type declaration */
+const person  = {
+    name: 'Aditya',
+    age: 29
+};
+
+console.log(person.name)
