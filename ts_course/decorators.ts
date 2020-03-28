da89f32b6e6141974cc46fdbceaa195a88151bf2
aditya uppuluri <uppuluriaditya@gmail.com>
2020-03-28 20:08:42 +0530

    Overriding Constructor

--- ts_course/decorators.ts
+++ ts_course/decorators.ts
@@ -20,3 +20,22 @@ class Greeter {
 
 let objGreet = new Greeter("OLA!!!");
 console.log(objGreet.greet());
+
+/** Override constructor */
+function classDecorator<T extends {new (...args: any[]): {}}>(constructor: T){
+    return class extends constructor{
+        newProperty = "New Property";
+        hello = "override";
+    }
+}
+
+@classDecorator
+class  Greeter_2{
+    property = "property";
+    hello: string;
+    constructor(m:string){
+        this.hello = m;
+    }
+}
+
+console.log(new Greeter_2("world!!"));
