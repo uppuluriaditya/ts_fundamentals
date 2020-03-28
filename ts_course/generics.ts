f842edfbcf199ebe2611e1c1af44bc9c6f8123ea
aditya uppuluri <uppuluriaditya@gmail.com>
2020-03-28 16:43:55 +0530

    Working with Generic Type Variables

--- ts_course/generics.ts
+++ ts_course/generics.ts
@@ -13,3 +13,13 @@ console.log(first_way_string);
 
 let second_way_string = identity("Aditya-2");
 console.log(second_way_string);
+
+function lastValue<T>(arg: T[]){
+    return arg[arg.length - 1]
+}
+
+let num = [1, 2, 3, 4, 5]
+let str = ["Aditya", "Mani", "Shrikant"]
+
+console.log(lastValue(num))
+console.log(lastValue(str))
