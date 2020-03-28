fec15ff041f96938ca1736dec77da0bfb875c36b
aditya uppuluri <uppuluriaditya@gmail.com>
2020-03-28 15:46:45 +0530

    Inheritence Changed Author

--- ts_course/app.ts
+++ ts_course/app.ts
@@ -12,5 +12,32 @@ class Animal{
     }
 }
 
-let animal = new Animal("tiger")
-animal.walk(10)
+// let animal = new Animal("tiger")
+// animal.walk(10)
+
+/** Inheritence */
+class Snake extends Animal{
+    /**
+     *
+     */
+    constructor(name: string) {
+        super(name);
+
+    }
+
+    walk(distance: number){
+        console.log("Snakes really don't walk")
+    }
+
+    hiss(numOfTimes: number){
+        let finalHiss:string = ""
+        for(let i=0; i<numOfTimes; i++){
+            finalHiss += " hisssssss!!!!"
+        }
+        console.log(finalHiss)
+    }
+}
+
+let rattle = new Snake("Rattle Snake");
+rattle.hiss(10)
+rattle.walk(10)
